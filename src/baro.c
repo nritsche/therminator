cd0f79a4f3bfdac1545e97deed84f2e3f56538cc
Rick Nitsche <ricknitsche@gmx.de>
2012-12-14 23:58:04 +0100

    pressure read

--- src/baro.c
+++ src/baro.c
@@ -1,7 +1,16 @@
 #include "baro.h"
 #include "spi.h"
 
+void init_baro()
+{
+	// set cs pins as output
+	SPI_CS_DIR |= ((1<<SPI_CS_PIN1) | (1<<SPI_CS_PIN2));
+}
+
 unsigned char get_pressure(unsigned short int pitot)
 {
-	return receive();
+	SPI_CS_PORT |= (1<<SPI_CS_PIN1);
+	unsigned char p = receive();
+	SPI_CS_PORT &= ~(1<<SPI_CS_PIN1);
+	return p;
 }
