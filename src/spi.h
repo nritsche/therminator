63d8798fca5705fb39a198a2cad49aaf1b50e00b
Rick Nitsche <ricknitsche@gmx.de>
2012-12-14 01:16:35 +0100

    spi

--- src/spi.h
+++ src/spi.h
@@ -1,11 +1,17 @@
 #ifndef SPI_H_
 #define SPI_H_
 
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <spi.h>
+
 #define SPI_PORT PORTB
 #define SPI_DIR DDRB
 #define SPI_CS_PIN PB4
 #define SPI_SCK_PIN PB7
 #define SPI_MOSI_PIN PB5
 #define SPI_MISO_PIN PB6
+
+#define SPI_CMD_SIZE 16
 
 #endif
