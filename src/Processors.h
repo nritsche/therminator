4a20158add1e5e9fec97fc6027e1bfa84d94cf73
Chuck Norris <chuck.norris@quantentunnel.de>
2012-12-14 23:04:13 +0100

    mega234p definitions

---
+++ src/Processors.h
@@ -0,0 +1,16 @@
+/*
+  Processors.h
+*/
+
+#if defined (__AVR_ATmega324P__)
+#define SPCR0 SPCR
+#define SPIE0 SPIE
+#define MSTR0 MSTR
+#define SPRO0 SPRO
+#define CPOL0 CPOL
+#define CPHA0 CPHA
+#define SPSR0 SPSR
+#define SPDR0 SPDR
+#define SPE0 SPE
+#define SPR00 SPR0
+#endif
